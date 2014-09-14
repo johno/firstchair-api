faadde49fdd12956176cc9a53262d2d71fd7a11d
John Otander <johnotander@gmail.com>
2014-09-14 12:19:32 -0600

    Add object key wrapper to resort api responses.

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -4,7 +4,7 @@ class ResortsController < ApplicationController
   def index
     @resorts = Resort.all
 
-    render json: @resorts
+    render json: { resorts: @resorts }
   end
 
   # GET /resorts/1
@@ -12,39 +12,6 @@ class ResortsController < ApplicationController
   def show
     @resort = Resort.find(params[:id])
 
-    render json: @resort
-  end
-
-  # POST /resorts
-  # POST /resorts.json
-  def create
-    @resort = Resort.new(params[:resort])
-
-    if @resort.save
-      render json: @resort, status: :created, location: @resort
-    else
-      render json: @resort.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /resorts/1
-  # PATCH/PUT /resorts/1.json
-  def update
-    @resort = Resort.find(params[:id])
-
-    if @resort.update(params[:resort])
-      head :no_content
-    else
-      render json: @resort.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /resorts/1
-  # DELETE /resorts/1.json
-  def destroy
-    @resort = Resort.find(params[:id])
-    @resort.destroy
-
-    head :no_content
+    render json: { resort: @resort }
   end
 end
